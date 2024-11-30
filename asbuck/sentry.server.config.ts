cf1996229c25ad35437e00d8cc3f7536c2fa14b7
Juan Siagian <jsa.siagian@gmail.com>
2024-12-01 00:24:29 +0700

    sentry config

--- asbuck/sentry.server.config.ts
+++ asbuck/sentry.server.config.ts
@@ -5,11 +5,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://28d1d82a5e85e68a1c26978840aaec0f@o4508388243800064.ingest.us.sentry.io/4508388249370624",
-
-  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 1,
-
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
+  dsn: process.env.SENTRY_DSN,
+  release: process.env.SENTRY_RELEASE || 'my-project@1.0.0',
+  serverName: process.env.SENTRY_URL,
 });
